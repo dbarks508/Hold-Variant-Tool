499543874668fe5b099ae489e9686da516557e1a
Dylan Barks <91198979+dbarks508@users.noreply.github.com>
2026-04-27 09:03:41 -0600

    adding hold variants

--- src/data/colors.jsx
+++ src/data/colors.jsx
@@ -20,4 +20,83 @@ export const colors = [
       absolute: "AB115-12",
     },
   },
+  {
+    name: "green",
+    codes: {
+      compx: "CX016-16",
+      absolute: "AB116-16",
+    },
+  },
+  {
+    name: "blue",
+    codes: {
+      compx: "CX213-01",
+      absolute: "AB213-01",
+    },
+  },
+  {
+    name: "purple",
+    codes: {
+      compx: "CX007-13",
+      absolute: "AB207-13",
+    },
+  },
+  {
+    name: "pink",
+    codes: {
+      compx: "CX211-26",
+      absolute: "AB211-26",
+    },
+  },
+  {
+    name: "black",
+    codes: {
+      compx: "CX018-01",
+      absolute: "AB018-01",
+    },
+  },
+  {
+    name: "white",
+    codes: {
+      compx: "CX012-01",
+      absolute: "AB112-01",
+    },
+  },
+  {
+    name: "fluorescent orange",
+    codes: {
+      compx: "CX014-11",
+    },
+  },
+  {
+    name: "fluorescent green",
+    codes: {
+      compx: "CX016-29",
+    },
+  },
+  {
+    name: "lime green",
+    codes: {
+      compx: "CX216-09",
+      absolute: "AB116-09",
+    },
+  },
+  {
+    name: "light purple",
+    codes: {
+      compx: "CX217-18",
+    },
+  },
+  {
+    name: "fluorescent pink",
+    codes: {
+      compx: "CX111-25",
+    },
+  },
+  {
+    name: "mint",
+    codes: {
+      absolute: "AB016-27",
+    },
+  },
 ];
